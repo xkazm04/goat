d0e3345ba2274618f47ce22f4a05b9cb3c6238fb
xkazm04 <kazdan@seznam.cz>
2026-01-26 23:37:35 +0100

    feat(02-02): add decorations barrel export
    - Exports Glow, Shimmer, GradientBorder components
    - Exports GRADIENT_PRESETS constant
    - Exports all associated types

---
+++ src/components/visual/decorations/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Decoration Components - Public Exports
+ *
+ * Import decorations from this index:
+ *   import { Glow, Shimmer, GradientBorder, GRADIENT_PRESETS } from '@/components/visual/decorations';
+ */
+
+export * from './Glow';
+export * from './Shimmer';
+export * from './GradientBorder';
